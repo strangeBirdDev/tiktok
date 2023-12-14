29ea0127cd1f77383963639c4e4aa17900bb6920
toanbdhe161559 <toanbdhe161559@fpt.edu.vn>
2023-12-15 00:43:47 +0700

    Base, style module, router and Header #1

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import logoTiktok from "@/assets/images/logo.svg";
+
+const images = {
+    logo: logoTiktok,
+};
+
+export default images;
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from "@/components/Layout";
+import { DefaultLayout } from "@/components/Layout";
+
+import Home from "@/pages/Home";
+import Following from "@/pages/Following";
+import Profile from "@/pages/Profile";
+import Upload from "@/pages/Upload";
+import Search from "@/pages/Search";
+
+// public route
+const publicRoutes = [
+    { path: "/", component: Home, layout: DefaultLayout },
+    { path: "/following", component: Following, layout: DefaultLayout },
+    { path: "/profile", component: Profile, layout: DefaultLayout },
+    { path: "/upload", component: Upload, layout: HeaderOnly },
+    { path: "/search", component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
